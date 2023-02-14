4205013b2c466ae178c538f3dc1bcd7ce2e12010
Glebokie <savostyk777@gmail.com>
2023-02-15 01:29:08 +0300

    init:fixed getQuotes js part 2

--- js-project/getquotes.js
+++ js-project/getquotes.js
@@ -1,5 +1,5 @@
 async function getQuotes() {  
-    const quotes = './data.json';
+    const quotes = './js-project/data.json';
     const res = await fetch(quotes);
     const data = await res.json(); 
     console.log(data);
