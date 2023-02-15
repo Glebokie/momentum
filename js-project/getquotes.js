8debfe690f99c5f02e12d17469a10d951678eec0
Glebokie <savostyk777@gmail.com>
2023-02-15 14:35:11 +0300

    init:update quotes list

--- js-project/getquotes.js
+++ js-project/getquotes.js
@@ -1,7 +1,25 @@
 async function getQuotes() {  
+    let bgNum;
+    const quoteEl = document.querySelector('.quote')
+    const authorEl = document.querySelector('.author')
+    const changeQuoteEl = document.querySelector('.change-quote')
     const quotes = './js-project/data.json';
     const res = await fetch(quotes);
     const data = await res.json(); 
-    console.log(data);
+    
+    function getRandomNum (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+   
+    function getRandomQuote() {
+    bgNum = getRandomNum(0, data.length - 1)
+    quoteEl.textContent = data[bgNum].text
+    authorEl.textContent = data[bgNum].author
+    }
+    getRandomQuote()
+    changeQuoteEl.addEventListener('click', getRandomQuote )
+
   }
   export default getQuotes;
