9a885c73e76cccff15addccb2de81d7a157799f1
Glebokie <savostyk777@gmail.com>
2023-02-19 12:48:15 +0300

    init:fixed bugs in audioplayer

--- js-project/playList.js
+++ js-project/playList.js
@@ -1,17 +1,17 @@
 const playList = [
     {      
       title: '50 Cent In Da Club',
-      src: '../assets/sounds/50 Cent In Da Club.mp3',
+      src: './assets/sounds/50 Cent In Da Club.mp3',
       duration: '00:58'
     },  
     {      
       title: '50 Cent Candy Shop',
-      src: '../assets/sounds/50 Cent — Candy Shop.mp3',
+      src: './assets/sounds/50 Cent — Candy Shop.mp3',
       duration: '03:50'
     },
     {      
         title: '50 Cent Just A Lil Bit',
-        src: '../assets/sounds/50 Cent — Just A Lil Bit.mp3',
+        src: './assets/sounds/50 Cent — Just A Lil Bit.mp3',
         duration: '03:50'
       },
   ]
